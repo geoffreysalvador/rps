4465ad8478dece359709583dd1f2a9c069ae010a
geoffreysalvador <16317958+geoffreysalvador@users.noreply.github.com>
2024-03-24 17:31:20 +0800

    Created UI.

--- script.js
+++ script.js
@@ -15,9 +15,11 @@ function playRound(playerSelection, computerSelection) {
                 break;
             case "paper":
                 result = "You win! Paper beats rock!";
+                playerScore += 1;
                 break;
             case "scissors":
                 result = "You lose! Rock beats scissors!";
+                computerScore += 1;
                 break;
         }
 
@@ -25,12 +27,14 @@ function playRound(playerSelection, computerSelection) {
         switch (player){
             case "rock":
                 result = "You lose! Paper beats rock!";
+                computerScore += 1;
                 break;
             case "paper":
                 result = "It's a tie!";
                 break;
             case "scissors":
                 result = "You win! Scissors beat paper!";
+                playerScore += 1;
                 break;
         }
 
@@ -38,9 +42,11 @@ function playRound(playerSelection, computerSelection) {
         switch (player){
             case "rock":
                 result = "You win! Rock beats scissors!";
+                playerScore += 1;
                 break;
             case "paper":
                 result = "You lose! Scissors beat paper!";
+                computerScore += 1;
                 break;
             case "scissors":
                 result = "It's a tie!";
@@ -51,6 +57,8 @@ function playRound(playerSelection, computerSelection) {
     return result;
 }
 
+
+/*
 function playGame() {
     console.log(playRound(playerSelection, computerSelection));
     console.log(playRound(playerSelection, computerSelection));
@@ -62,3 +70,84 @@ function playGame() {
 const playerSelection = prompt("Please make your choice");
 const computerSelection = getComputerChoice();
 console.log(playGame());
+
+*/
+
+let playerScore = 0;
+let computerScore = 0;
+
+const dispPlayScore = document.querySelector('#player');
+const dispCompScore = document.querySelector('#computer');
+
+const rockBtn = document.querySelector('#rock');
+rockBtn.addEventListener('click', function() {
+    let playerSelection = 'rock';
+    let computerSelection = getComputerChoice();
+
+    let result = playRound(playerSelection, computerSelection);
+
+    const text = document.querySelector('#result');
+    text.textContent = result;
+
+    dispPlayScore.textContent = playerScore;
+    dispCompScore.textContent = computerScore;
+
+    if (playerScore === 5) {
+        alert("You win!");
+        location.reload();
+    }
+    
+    if (computerScore === 5) {
+        alert('You lose!');
+        location.reload();
+    }
+})
+
+const paperBtn = document.querySelector('#paper');
+paperBtn.addEventListener('click', function() {
+    let playerSelection = 'paper';
+    let computerSelection = getComputerChoice();
+
+    let result = playRound(playerSelection, computerSelection);
+
+    const text = document.querySelector('#result');
+    text.textContent = result;
+
+    dispPlayScore.textContent = playerScore;
+    dispCompScore.textContent = computerScore;
+
+    if (playerScore === 5) {
+        alert("You win!");
+        location.reload();
+    }
+    
+    if (computerScore === 5) {
+        alert('You lose!');
+        location.reload();
+    }
+})
+
+const scissorsBtn = document.querySelector('#scissors');
+scissorsBtn.addEventListener('click', function() {
+    let playerSelection = 'scissors';
+    let computerSelection = getComputerChoice();
+
+    let result = playRound(playerSelection, computerSelection);
+
+    const text = document.querySelector('#result');
+    text.textContent = result;
+
+    dispPlayScore.textContent = playerScore;
+    dispCompScore.textContent = computerScore;
+
+    if (playerScore === 5) {
+        alert("You win!");
+        location.reload();
+    }
+    
+    if (computerScore === 5) {
+        alert('You lose!');
+        location.reload();
+    }
+})
+
